f053bca937897314e19b232d0c20f7a2a4667c23
Kevinoliver118 <kevinoliver2f@gmail.com>
2024-10-25 12:59:19 -0500

    primer comentario

--- js/formulario.js
+++ js/formulario.js
@@ -2,11 +2,11 @@ const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 
 const expresiones = {
-	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	password: /^.{4,12}$/, // 4 a 12 digitos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+	telefono: /^\d{7,14}$/,
+	direccion: /^[a-zA-Z0-9\_\-]{4,16}$/
 }
 
 const campos = {
@@ -14,7 +14,8 @@ const campos = {
 	nombre: false,
 	password: false,
 	correo: false,
-	telefono: false
+	telefono: false,
+	direccion:false
 }
 
 const validarFormulario = (e) => {
@@ -37,6 +38,9 @@ const validarFormulario = (e) => {
 		break;
 		case "telefono":
 			validarCampo(expresiones.telefono, e.target, 'telefono');
+		break;
+		case "direccion":
+			validarCampo(expresiones.direccion, e.target, 'direccion');
 		break;
 	}
 }
